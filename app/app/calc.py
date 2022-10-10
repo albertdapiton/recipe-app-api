a25215ec26eed18c0f51b0374cfdf78a9e7426b3
Albert Dapiton <albert_dapiton@cody.inc>
2022-10-10 19:17:16 +0800

    Django Rest Framework

---
+++ app/app/calc.py
@@ -0,0 +1,13 @@
+"""
+Calculator Functions
+"""
+
+
+def add(x, y):
+    """Add x and y and return result."""
+    return x + y
+
+
+def substract(x, y):
+    """Subtract x from y and return result."""
+    return y - x
